b2d21bdb2765c8c2af6a91616d4129b43cfbe15f
Mega343 <mega9594@Rambler.ru>
2016-06-08 19:14:35 +0300

    Update Calculate.java

--- src/com/goit/gojavaonline/collections/Calculate.java
+++ src/com/goit/gojavaonline/collections/Calculate.java
@@ -10,39 +10,6 @@ public class Calculate {
     SetEfficiency set = new SetEfficiency();
     List<Long> time;
 
-    public static void main(String[] args) {
-        Calculate calculate = new Calculate();
-        List<Integer> arrayList = new ArrayList<>();
-        List<Integer> linkedList = new LinkedList<>();
-        Set<Integer> hashSet = new HashSet<>();
-        Set<Integer> treeSet = new TreeSet<>();
-        System.out.println("Populate ArrayList for 10000: " + calculate.listPopulateEfficiency(arrayList, 10000));
-        System.out.println("Add in ArrayList for 10000: " + calculate.listAddEfficiency(arrayList));
-        System.out.println("Contains in ArrayList 10000: " + calculate.listContainsEfficiency(arrayList));
-        System.out.println("Get ArrayList for 10000: " + calculate.listGetEfficiency(arrayList));
-        System.out.println("Remove from ArrayList 10000: " + calculate.listRemoveEfficiency(arrayList));
-        System.out.println("Iterator Add ArrayList for 10000: " + calculate.listIteratorAddEfficiency(arrayList,10000));
-        System.out.println("Iterator remove ArrayList for 10000: " + calculate.listIteratorRemoveEfficiency(arrayList));
-        System.out.println();
-        System.out.println("Populate LinkedList for 10000: " + calculate.listPopulateEfficiency(linkedList, 10000));
-        System.out.println("Add in LinkedList for 10000: " + calculate.listAddEfficiency(linkedList));
-        System.out.println("Contains in LinkedList 10000: " + calculate.listContainsEfficiency(linkedList));
-        System.out.println("Get LinkedList for 10000: " + calculate.listGetEfficiency(linkedList));
-        System.out.println("Remove from LinkedList 10000: " + calculate.listRemoveEfficiency(linkedList));
-        System.out.println("Iterator Add LinkedList for 10000: " + calculate.listIteratorAddEfficiency(linkedList,10000));
-        System.out.println("Iterator remove LinkedList for 10000: " + calculate.listIteratorRemoveEfficiency(linkedList));
-        System.out.println();
-        System.out.println("Hash set populate for 10000 " + calculate.setPopulateEfficiency(hashSet, 10000));
-        System.out.println("Hash set add for 10000 " + calculate.setAddEfficiency(hashSet));
-        System.out.println("Hash set contains for 10000 " + calculate.setContainsEfficiency(hashSet));
-        System.out.println("Hash set remove for 10000 " + calculate.setRemoveEfficiency(hashSet));
-        System.out.println();
-        System.out.println("Tree set populate for 10000 " + calculate.setPopulateEfficiency(treeSet, 10000));
-        System.out.println("Tree set add for 10000 " + calculate.setAddEfficiency(treeSet));
-        System.out.println("Tree set contains for 10000 " + calculate.setContainsEfficiency(treeSet));
-        System.out.println("Tree set remove for 10000 " + calculate.setRemoveEfficiency(treeSet));
-    }
-
     public long listPopulateEfficiency(List<Integer> someList, int n){
         time = new ArrayList<>();
         long result;
